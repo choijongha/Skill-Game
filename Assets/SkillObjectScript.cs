baa0a767be7b23a0d269f65fbe427ce1828db71c
choijongha <80575094+choijongha@users.noreply.github.com>
2022-04-19 21:45:18 +0900

    스킬 오브젝트와 스킬이미지 연결

--- Assets/SkillObjectScript.cs
+++ Assets/SkillObjectScript.cs
@@ -6,13 +6,10 @@ public class SkillObjectScript : MonoBehaviour
     [SerializeField] Transform skillImageTh;
     public GameObject skill;
     public GameObject skills;
-
-    private void Update()
+    public GameObject InstantiateSkill()
     {
-        // 클릭하면 skill 이미지를 이미지 자리에 복사하고 skills 오브젝트 부모를 가진다.
-        if (onClicked)
-        {
-            GameObject instantiateSkill = Instantiate(skill, skillImageTh.position, transform.rotation, skills.transform);
-        }
+        GameObject instantiateSkill = Instantiate(skill, skillImageTh.position, transform.rotation, skills.transform);
+        return instantiateSkill;
     }
+    
 }
--- Assets/SkillUnit.cs
+++ Assets/SkillUnit.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 public class SkillUnit : MonoBehaviour
 {
     [SerializeField] GameObject skillPanel;
@@ -6,10 +10,19 @@ public class SkillUnit : MonoBehaviour
     private SkillObjectScript skillObject;
     private Camera mainCamera;
     private RaycastHit2D hit;
+
+    private GraphicRaycaster mRaycaster;
+    private PointerEventData mPointerEventData;
+    private EventSystem mEventSystem;
+    GameObject skillInstantiation;
     private void Awake()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         skillObject = GameObject.FindGameObjectWithTag("SkillObject").GetComponent<SkillObjectScript>();
+        
+        mRaycaster = GameObject.Find("Canvas").GetComponent<GraphicRaycaster>();
+        mEventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
     }
     private void Update()
     {
@@ -19,27 +32,48 @@ public class SkillUnit : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            UIClick();
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             hit = Physics2D.Raycast(worldPoint, Vector2.zero);
             if(hit.collider != null)
             {
                 Debug.Log(hit.collider.name);
-                if (hit.collider.tag == "SkillObject")
+                SkillObjectScript skillObjectScript = GameObject.Find(hit.collider.name).GetComponent<SkillObjectScript>();
+                
+                if (!skillObjectScript.onClicked)
                 {
-                    if (!skillObject.onClicked)
-                    {
-                        skillObject.onClicked = true;
-                        skillPanel.SetActive(true);
-                    }
-                    else
-                    {
-                        skillObject.onClicked = false;
-                        skillPanel.SetActive(false);
-                    }
+                    skillObjectScript.onClicked = true;
+                    skillPanel.SetActive(true);
+                    skillInstantiation = skillObjectScript.InstantiateSkill();
+                }
+                else
+                {
+                    skillObjectScript.onClicked = false;
+                    skillPanel.SetActive(false);
+                    Destroy(skillInstantiation);
                 }
             }
-            
         }
     }
+    private void UIClick()
+    {
+        //Set up the new Pointer Event
+        mPointerEventData = new PointerEventData(mEventSystem);
+        //Set the Pointer Event Position to that of the mouse position
+        mPointerEventData.position = Input.mousePosition;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        mRaycaster.Raycast(mPointerEventData, results);
+
+        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+        foreach (RaycastResult result in results)
+        {
+            Debug.Log("Hit " + result.gameObject.name);
+        }
+    }
+
     
 }
